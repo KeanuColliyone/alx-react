53f660c5052519f285ccaa139a74b9343d19b53e
Kenan Kotzee <frostcolliyone@gmail.com>
2024-11-01 16:09:53 +0200

    0x01-react_intro

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import holbertonLogo from './holberton_logo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={holbertonLogo} alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <main className="App-body">
+        <p>Login to access the full dashboard</p>
+      </main>
+      <footer className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
